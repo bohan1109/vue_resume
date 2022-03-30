3b8422220ccbc8cb2b70b045a8199432910eb9c6
bohan1109 <4A790162@stust.edu.tw>
2022-03-30 10:16:08 +0800

    fix: :bug: fix3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,11 +11,12 @@
         </p>
         <p class="content">
           <a href="https://github.com/bohan1109">
-          <img
-            class="img"
-            src="https://www.svgrepo.com/show/341847/github.svg"
-            href="https://github.com/bohan1109"
-          />bohan1109</a>
+            <img
+              class="img"
+              src="https://www.svgrepo.com/show/341847/github.svg"
+              href="https://github.com/bohan1109"
+            />bohan1109</a
+          >
         </p>
         <p class="content">
           <img
@@ -24,14 +25,17 @@
           />qazwsx147866@gmail.com
         </p>
       </div>
-      
-        <img
-          src="../assets/4A790162-2.jpg"
-          alt=""
-          class="mt-5"
-          style="width:120px;height:150px;border:1px solid rgb(215, 215, 216)"
-        />
-      
+
+      <img
+        src="../assets/4A790162-2.jpg"
+        alt=""
+        class="mt-5"
+        style="
+          width: 120px;
+          height: 150px;
+          border: 1px solid rgb(215, 215, 216);
+        "
+      />
     </div>
     <hr class="hr" />
     <div class="summary">
@@ -56,7 +60,7 @@
       <p class="content">
         在大四下時，到創科資訊股份有限公司培訓進行培訓，在這邊學習許多技能，從設計、前端、後端、測試、部屬都有學到，對專案的流程及執行有了更進一步的了解，
         在實習時有許多實作機會，可以讓我們進行團隊專案的製作，在團隊專案製作中，除了學到許多沒用過的技術，像是Laravel、React、Git、虛擬環境等等，
-        還學到了與團隊成員溝通的技巧，因為與不同的人分組合作，有同樣為資管系的台中科技大學的幾位、逢甲大學資工系的、雲科大資管系還有嘉義大學設計系的同學，
+        還學到了與團隊成員溝通的技巧，因為與不同的人分組合作，有同樣為資管系的台中科技大學的幾位、逢甲大學資工系的、雲科大資管系還有嘉義大學數位設計與管理學系的同學，
         養成了與不同的人合作時的溝通能力。
       </p>
     </div>
@@ -127,8 +131,7 @@
     <hr class="hr" />
     <div class="Certificates">
       <p class="title">證照 Certificates</p>
-      
-        <div class="Cergrid grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 p-5 w-full">
+      <div class="Cergrid">
         <li>電腦軟體應用丙級</li>
         <li>電腦軟體應用乙級</li>
         <li>會計事務-資訊丙級</li>
@@ -139,11 +142,6 @@
         <li>企業電子化人才能力鑑定單科合格證書</li>
         <li>Planner Of Enterprise Resource Planning</li>
       </div>
-
-      
-
-      
-
     </div>
     <hr class="hr" />
     <div class="project">
@@ -151,7 +149,6 @@
       <div class="projectdiv">
         <div class="basic">
           <div class="left">
-            
             <p class="projectname">Monospace鎖櫃系統</p>
             <p class="projectcontent">
               公司新進鎖櫃，為了決定鎖櫃使用者，鎖櫃使用者需要輸入志願並抽籤，因此我們製作了個鎖櫃系統，此系統前端是使用了react.js後端使用了laravel，
@@ -159,12 +156,7 @@
             </p>
           </div>
           <div class="picture">
-            <img
-              src="../assets/monolock.png"
-              alt=""
-              class="pricture"
-              
-            />
+            <img src="../assets/monolock.png" alt="" class="pricture" />
           </div>
         </div>
         <div class="basic">
@@ -172,16 +164,12 @@
             <p class="projectname">即時溫溼度感測</p>
             <p class="projectcontent">
               這個專案是做溫溼度感測，並將資料即時存進資料庫，並將資料庫內容抓出來，將資料顯示在網頁上，讓觀測人員可以使用透過網頁的圖表，
-              隨時知道現在的溫溼度如何，我負責的部分是mqtt broker的架設、python flask的api製作，包含資料庫連線、表格製作、新增、搜尋等功能。
+              隨時知道現在的溫溼度如何，我負責的部分是mqtt broker的架設、python
+              flask的api製作，包含資料庫連線、表格製作、新增、搜尋等功能。
             </p>
           </div>
-          
-            <img
-              src="../assets/1028905.jpg"
-              alt=""
-              class="picture"
-            />
-          
+
+          <img src="../assets/1028905.jpg" alt="" class="picture" />
         </div>
       </div>
     </div>
@@ -201,7 +189,7 @@
     <hr class="hr" />
     <div class="language">
       <p class="title">語言能力 Language</p>
-      <p class="content">中文(母語)<br>英文(初學)</p>
+      <p class="content">中文(母語)<br />英文(初學)</p>
     </div>
   </div>
 </template>
@@ -228,10 +216,12 @@ div #resume {
 .skilldiv hr {
   @apply my-2;
 }
-.projectdiv{
+.projectdiv {
   @apply pl-5;
 }
-
+.Cergrid {
+  @apply grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 p-5 w-full;
+}
 .img {
   width: 30px;
   height: 30px;
@@ -254,7 +244,7 @@ div #resume {
   @apply text-3xl mt-5 ml-4;
 }
 .projectname {
-  @apply text-2xl mt-4 ml-5 ;
+  @apply text-2xl mt-4 ml-5;
 }
 .projectcontent {
   @apply mt-4 ml-7;
@@ -278,21 +268,20 @@ div #resume {
 .basic {
   @apply flex w-full justify-between;
 }
-.left{
+.left {
   @apply w-8/12 relative flex-col break-all;
 }
-.picture{
+.picture {
   @apply w-40 h-40 pt-9 items-center;
 }
-.projectname::before{
-  content:"";
+.projectname::before {
+  content: "";
   height: 1.5rem;
   width: 1.5rem;
   @apply rounded-full absolute -left-4 mt-1 z-10 border-0 bg-cyan-100;
-
 }
-.projectcontent::before{
-  content:"";
+.projectcontent::before {
+  content: "";
   left: -6.5px;
   @apply rounded absolute mt-1 w-1 sm:h-4/5 md:h-4/5 xl:h-3/5 2xl:h-3/5 lg:h-3/5 z-10 border-0 bg-cyan-100;
 }
